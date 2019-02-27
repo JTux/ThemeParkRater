b31c85c58a373aded217b4b8d50d1d1eef0e7610
JTux <the1jtux@gmail.com>
2019-02-27 18:37:19 -0500

    Controllers Finished

---
+++ ThemeParkRater.WebAPI/Controllers/ThemeParkController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ThemeParkRater.Models.ThemeParkModels;
+using ThemeParkRater.Services;
+
+namespace ThemeParkRater.WebAPI.Controllers
+{
+    public class ThemeParkController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            var service = new ThemeParkService();
+            var park = service.GetThemeParks();
+            return Ok(park);
+        }
+
+        public IHttpActionResult GetById(int id)
+        {
+            var service = new ThemeParkService();
+            var park = service.GetParkByID(id);
+            return Ok(park);
+        }
+
+        public IHttpActionResult Post(ThemeParkCreate park)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = new ThemeParkService();
+
+            if (!service.CreateThemePark(park))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Put(ThemeParkEdit park)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = new ThemeParkService();
+
+            if (!service.EditThemePark(park))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = new ThemeParkService();
+
+            if (!service.DeleteThemePark(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+}
---
+++ ThemeParkRater.WebAPI/Controllers/ThemeParkRatingController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ThemeParkRater.Models.ThemeParkRatingModels;
+using ThemeParkRater.Services;
+
+namespace ThemeParkRater.WebAPI.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/ThemeParkRating")]
+    public class ThemeParkRatingController : ApiController
+    {
+
+        [Route("ByParkID/{id:int}")]
+        public IHttpActionResult GetByParkId(int id)
+        {
+            var service = GetRatingService();
+            var ratings = service.GetRatingsByParkID(id);
+            return Ok(ratings);
+        }
+
+        [Route("ByRatingID/{id:int}")]
+        public IHttpActionResult GetById(int id)
+        {
+            var service = GetRatingService();
+            var park = service.GetRatingByID(id);
+            return Ok(park);
+        }
+
+        public IHttpActionResult Post(ThemeParkRatingCreate rating)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = GetRatingService();
+
+            if (!service.CreateRating(rating))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Put(ThemeParkRatingEdit rating)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = GetRatingService();
+
+            if (!service.EditThemeParkRating(rating))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = GetRatingService();
+
+            if (!service.DeleteThemeParkRating(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        private ThemeParkRatingService GetRatingService()
+        {
+            return new ThemeParkRatingService(Guid.Parse(User.Identity.GetUserId()));
+        }
+    }
+}
